d395fffeb89a71a18069144c3b821de0c255a70b
ryancambray <cambrayryan@gmail.com>
2020-10-10 15:56:09 +0100

    Remove unnecessary set methods in LastFmProperties.java class

--- src/main/java/com/stats/cambot/config/LastFmProperties.java
+++ src/main/java/com/stats/cambot/config/LastFmProperties.java
@@ -16,31 +16,16 @@ public class LastFmProperties {
         return apiKey;
     }
 
-    public void setApiKey(String apiKey) {
-        this.apiKey = apiKey;
-    }
-
     public String getApiSecret() {
         return apiSecret;
-    }
-
-    public void setApiSecret(String apiSecret) {
-        this.apiSecret = apiSecret;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
